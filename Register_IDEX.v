6edcd1b7ebffdcaa7d786e77013f8154cb946fd5
JTKiew <45333715+JTKiew@users.noreply.github.com>
2020-12-12 13:27:08 +0800

    Update Register_IDEX.v

--- Register_IDEX.v
+++ Register_IDEX.v
@@ -1,98 +1,111 @@
-module Register_IDEX (
-	clk_i, 
-	start_i,
+`define R_Type 2'b10
+`define I_Type 2'b00
+`define S_Type 2'b01
+`define SB_Type 2'b11
 
-	// Instruction Data & Immediate & funct & Instruction Address 
-	RS1Data_i, 
-	RS2Data_i,
-	SignExtended_i,	
-	funct_i,		// 10bits instr[31:25,14:12]	funct 3 and funct 7	 
-	RS1_Addr_i,		// 5 bits instr[19:15]			rs1 					
-	RS2_Addr_i,		// 5 bits instr[24:20]			rs2 					
-	Rd_Addr_i,		// 5 bits instr[11:7]			rd
-
-	RS1Data_o,
-	RS2Data_o,
-	SignExtended_o,
-	funct_o,
-	RS1_Addr_o,		
-	RS2_Addr_o,
-	Rd_Addr_o,
-
-	// Control 
-	RegWrite_i,	
-	MemToReg_i,	
-	MemRead_i,	
-	MemWrite_i,	
-	ALUOp_i,	//2 bits
-	ALUSrc_i,	
-
+module Control(
+	Op_i,		
+	NoOp_i,	
 	RegWrite_o,
-	MemToReg_o,
+	MemtoReg_o,
 	MemRead_o,
 	MemWrite_o,
-	ALUOp_o,
-	ALUSrc_o
+	ALUOp_o,	
+	ALUSrc_o,
+	Branch_o
 );
 
-input clk_i, start_i;
-input [31:0] RS1Data_i, RS2Data_i, SignExtended_i;
-input [9:0] funct_i;
-input [4:0] RS1Addr_i, RS2Addr_i,Rd_Addr_i;
+// Ports
+input 	[6:0]		Op_i;	
+input NoOp_i;	
 
-input [1:0] ALUOp_i;
-input RegWrite_i, MemToReg_i, MemRead_i, MemWrite_i, ALUSrc_i;	
+output RegWrite_o, MemtoReg_o, MemRead_o, MemWrite_o, ALUSrc_o, Branch_o;
+output 	[1:0]		ALUOp_o;
 
-output [31:0] RS1Data_o, RS2Data_o, SignExtended_o;
-output [9:0] funct_o;
-output [4:0] RS1Addr_o, RS2Addr_o,Rd_Addr_o;
+reg RegWrite_o, MemtoReg_o, MemRead_o, MemWrite_o, ALUSrc_o, Branch_o;
+reg 	[1:0]		ALUOp_o;
 
-output [1:0] ALUOp_o;
-output RegWrite_o, MemToReg_o, MemRead_o, MemWrite_o, ALUSrc_o;	
+always @(Op_i or NoOp_i) begin
+	// Phase 1.4
+	if (NoOp_i) begin
+		RegWrite_o	= 1'b0;
+		MemtoReg_o	= 1'b0;
+		MemRead_o	= 1'b0;
+		MemWrite_o	= 1'b0;
+		ALUOp_o		= `I_Type;
+		ALUSrc_o	= 1'b0;
+		Branch_o	= 1'b0;	
+	end
 
-reg [31:0] RS1Data_o, RS2Data_o, SignExtended_o;
-reg [9:0] funct_o;
-reg [4:0] RS1Addr_o, RS2Addr_o,Rd_Addr;
-
-reg [1:0] ALUOp_o;
-reg RegWrite_o, MemToReg_o, MemRead_o, MemWrite_o, ALUSrc_o;	
-
-always @(posedge clk_i) begin
-	if (start_i) begin 
-		RS1Data_o 		    <= RS1Data_i;
-		RS2Data_o 		    <= RS2Data_i;
-		SignExtended_o 	    <= SignExtended_i;
-		RS1Addr_o			<= RS1Addr_i;
-		RS2Addr_o			<= RS2Addr_i;
-		funct_o				<= funct_i;
-		Rd_Addr_o       	<= Rd_Addr_i;
-	
-		RegWrite_o          <= RegWrite_i; 
-		MemToReg_o          <= MemToReg_i;
-		MemRead_o           <= MemRead_i; 
-		MemWrite_o          <= MemWrite_i;
-		ALUOp_o             <= ALUOp_i;
-		ALUSrc_o            <= ALUSrc_i;
-
-	end 
 	else begin
-		RS1Data_o 		    <= RS1Data_o;
-		RS2Data_o 		    <= RS2Data_o;
-		SignExtended_o 	    <= SignExtended_o;
-		RS1Addr_o			<= RS1Addr_o;
-		RS2Addr_o			<= RS2Addr_o;
-		funct_o				<= funct_o;
-		Rd_Addr_o           <= Rd_Addr_o;
-	
-		RegWrite_o          <= RegWrite_o; 
-		MemToReg_o          <= MemToReg_o;
-		MemRead_o           <= MemRead_o; 
-		MemWrite_o          <= MemWrite_o;
-		ALUOp_o             <= ALUOp_o;
-		ALUSrc_o            <= ALUSrc_o;
-	end 
+		case(Op_i)
+			// R-Type (Arithmetic)
+			7'b0110011: begin
+				RegWrite_o	= 1'b1;
+				MemtoReg_o	= 1'b0;
+				MemRead_o	= 1'b0;
+				MemWrite_o	= 1'b0;
+				ALUOp_o		= `R_Type;
+				ALUSrc_o	= 1'b0;
+				Branch_o	= 1'b0;
+			end
+
+			// I-Type (Immediate Arithmetic) 
+			7'b0010011: begin
+				RegWrite_o	= 1'b1;
+				MemtoReg_o	= 1'b0;
+				MemRead_o	= 1'b0;
+				MemWrite_o	= 1'b0;
+				ALUOp_o		= `I_Type;
+				ALUSrc_o	= 1'b1;
+				Branch_o	= 1'b0;
+			end
+
+			// I-Type (Load) 
+			7'b0000011: begin
+				RegWrite_o	= 1'b1;
+				MemtoReg_o	= 1'b1;
+				MemRead_o	= 1'b1;
+				MemWrite_o	= 1'b0;
+				ALUOp_o		= `I_Type;
+				ALUSrc_o	= 1'b1;
+				Branch_o	= 1'b0;
+			end
+
+			// S-Type (Store)
+			7'b0100011: begin
+				RegWrite_o	= 1'b0;
+				MemtoReg_o	= 1'bx;
+				MemRead_o	= 1'b0;
+				MemWrite_o	= 1'b1;
+				ALUOp_o		= `S_Type;
+				ALUSrc_o	= 1'b1;
+				Branch_o	= 1'b0;
+			end
+
+			// SB-Type (Branch)
+			7'b1100011: begin
+				RegWrite_o	= 1'b0;
+				MemtoReg_o	= 1'bx;
+				MemRead_o	= 1'b0;
+				MemWrite_o	= 1'b0;
+				ALUOp_o		= `SB_Type;
+				ALUSrc_o	= 1'b0;
+				Branch_o	= 1'b1;
+			end
+
+			defaut: begin
+				RegWrite_o	= 1'b0;
+				MemtoReg_o	= 1'b0;
+				MemRead_o	= 1'b0;
+				MemWrite_o	= 1'b0;
+				ALUOp_o		= `I_Type;
+				ALUSrc_o	= 1'b0;
+				Branch_o	= 1'b0;
+			end
+		endcase	
+	end
+
 end
 
 endmodule
-
-
