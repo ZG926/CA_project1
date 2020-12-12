9c3a88779a0884eb618f5914564aff77ee31c6db
JTKiew <45333715+JTKiew@users.noreply.github.com>
2020-12-12 22:32:23 +0800

    Update Adder.v

--- Adder.v
+++ Adder.v
@@ -1,14 +1,14 @@
 module Adder(
-    data1_in, 	// 32 bits
-    data2_in,	// 32 bits
-    data_o		// 32 bits
+    data1_i, 
+    data2_i,
+    data_o	
 );
 
 // Ports
-input 	[31:0]		data1_in;
-input 	[31:0]		data2_in;
+input 	[31:0]		data1_i;
+input 	[31:0]		data2_i;
 output 	[31:0]		data_o;
 
-assign data_o = data1_in + data2_in;
+assign data_o = data1_i + data2_i;
 
 endmodule
