a63332f8fffb488beda5939c5a2a422e69f2bf9c
JTKiew <45333715+JTKiew@users.noreply.github.com>
2020-12-12 13:26:39 +0800

    Update Register_EXMEM.v

--- Register_EXMEM.v
+++ Register_EXMEM.v
@@ -28,14 +28,13 @@ input   [31:0]      ALU_Result_i, MemWrite_Data_i;
 input   [4:0]       Rd_Addr_i;
 input RegWrite_i, MemToReg_i, MemRead_i, MemWrite_i;  
 
-output               clk_i,start_i;
-output   [31:0]      ALU_Result_i, MemWrite_Data_i;
-output   [4:0]       Rd_Addr_i;
-output RegWrite_i, MemToReg_i, MemRead_i, MemWrite_i;  
+output   [31:0]      ALU_Result_0, MemWrite_Data_0;
+output   [4:0]       Rd_Addr_0;
+output RegWrite_0, MemToReg_0, MemRead_0, MemWrite_0;  
 
-reg   [31:0]      ALU_Result_i, MemWrite_Data_i;
-reg   [4:0]       Rd_Addr_i;
-reg RegWrite_i, MemToReg_i, MemRead_i, MemWrite_i;  
+reg   [31:0]      ALU_Result_o, MemWrite_Data_o;
+reg   [4:0]       Rd_Addr_o;
+reg RegWrite_o, MemToReg_o, MemRead_o, MemWrite_o;  
 
 always @(posedge clk_i) begin
     if(start_i) begin
